3c3d25d617d30511922d2fecef032f8d51b72bea
mjz <112626877+Mjz86@users.noreply.github.com>
2024-11-03 14:41:54 -0800

    some reminders

--- WelcomeSillies.c
+++ WelcomeSillies.c
@@ -3,6 +3,7 @@
 int main()
 {
     printf("Hello Sillies\n");
-    printf("Welcome to my Soul Society >:3");
+    printf("Welcome to my Soul Society >:3\n");
+    printf("wanna watch the rest of the video? \n im a bit sleepy :3 ");
     return 0;
 }
